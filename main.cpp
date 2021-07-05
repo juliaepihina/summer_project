99e37bf414146a7a963161d799642fcc38190e6d
juliaepihina <86781452+juliaepihina@users.noreply.github.com>
2021-07-05 19:13:32 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+struct node
+{
+    char c;
+    int v;
+};
+
+std::vector<int> nums_used(10);
+std::vector<node*> nodes_used;
+
+bool same_nodes(node* nd1, node* nd2, int count)
+{
+    for (int i = 0; i < count; i++)
+        if (nd1[i].c != nd2[i].c || nd1[i].v != nd2[i].v)
+            return false;
+    return true;
+}
+
+bool is_used(node* nd, int count)
+{
+    for (auto i : nodes_used)
+        if (same_nodes(i, nd, count))
+            return true;
+    return false;
+}
+
+bool check(node* node_array, const int count, std::string first, std::string second, std::string result)
+{
+    int first_num = 0, second_num = 0, result_num = 0;
+    int m = 1, j, i;
+
+    for (i = first.length() - 1; i >= 0; i--)
+    {
+        char ch = first[i];
+        for (j = 0; j < count; j++)
+            if (node_array[j].c == ch)
+                break;
+
+        first_num += m * node_array[j].v;
+        m *= 10;
+    }
+    m = 1;
+
+    for (i = second.length() - 1; i >= 0; i--)
+    {
+        char ch = second[i];
+        for (j = 0; j < count; j++)
+            if (node_array[j].c == ch)
+                break;
+
+        second_num += m * node_array[j].v;
+        m *= 10;
+    }
+    m = 1;
+
+    for (i = result.length() - 1; i >= 0; i--)
+    {
+        char ch = result[i];
+        for (j = 0; j < count; j++)
+            if (node_array[j].c == ch)
+                break;
+
+        result_num += m * node_array[j].v;
+        m *= 10;
+    }
+
+    if (result_num == (first_num + second_num))
+        return true;
+
+    return false;
+}
+
+bool permutation(const int count, node* node_array, int n, std::string first, std::string second, std::string result)
+{
+    if (n == count - 1)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (nums_used[i] == 0)
+            {
+                node_array[n].v = i;
+                if (check(node_array, count, first, second, result) == 1)
+                {
+                    for (int j = 0; j < count; j++)
+                        std::cout << " " << node_array[j].c << " = "
+                        << node_array[j].v << std::endl;
+                    nodes_used.push_back(node_array);
+                    std::cout << std::endl;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (nums_used[i] == 0)
+        {
+            node_array[n].v = i;
+            nums_used[i] = 1;
+
+            if (permutation(count, node_array, n + 1, first, second, result) && !is_used(node_array, count))
+                return true;
+
+            nums_used[i] = 0;
+        }
+    }
+    return false;
+}
+
+bool solve(std::string first, std::string second, std::string result)
+{
+    int count = 0;
+
+    int l1 = first.length();
+    int l2 = second.length();
+    int l3 = result.length();
+
+    std::vector<int> freq(26);
+
+    for (int i = 0; i < l1; i++)
+        freq[first[i] - 'A']++;
+
+    for (int i = 0; i < l2; i++)
+        freq[second[i] - 'A']++;
+
+    for (int i = 0; i < l3; i++)
+        freq[result[i] - 'A']++;
+
+    for (int i = 0; i < 26; i++)
+        if (freq[i] > 0)
+            count++;
+
+    if (count > 10)
+        return 0;
+
+    node* node_array = new node[count];
+
+    for (int i = 0, j = 0; i < 26; i++)
+    {
+        if (freq[i] > 0)
+        {
+            node_array[j].c = char(i + 'A');
+            j++;
+        }
+    }
+    return permutation(count, node_array, 0, first, second, result);
+}
+
+int main()
+{
+    std::string first;
+    std::string second;
+    std::string result;
+    std::ifstream f("test.txt");
+    f >> first;
+    f >> second;
+    f >> result;
+    while (solve(first, second, result))
+        ;
+    if (nodes_used.empty()) {
+        std::cout << "No solution";
+    }
+    return 0;
+}
